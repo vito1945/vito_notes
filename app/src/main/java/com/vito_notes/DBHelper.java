3d485050535850c34e10cac1fac98e6afc2e680d
vitoriab <evitoria@gmail.com>
2016-01-15 15:36:52 -0600

    Funcionalidad para llamar Activities lista

---
+++ app/src/main/java/com/vito_notes/DBHelper.java
@@ -0,0 +1,80 @@
+package com.vito_notes;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Hashtable;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by vitoriab on 1/15/2016.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "VitoNotes.db";
+    public static final String CONTACTS_TABLE_NAME = "notes";
+
+
+    public DBHelper(Context context)
+    {
+        super(context, DATABASE_NAME , null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL(
+                "create table notes " +
+                        "(id integer primary key,note text, date text)"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+        db.execSQL("DROP TABLE IF EXISTS notes");
+        onCreate(db);
+    }
+
+    public boolean insertNote  (String note)
+    {
+        Calendar today = Calendar.getInstance();
+        today.set(Calendar.HOUR_OF_DAY,0);
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("note", note);
+        contentValues.put("date", today.toString());
+        db.insert("notes", null, contentValues);
+        return true;
+    }
+
+    public Cursor getData(int id){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from notes where id="+id+"", null );
+        return res;
+    }
+
+    public int numberOfRows(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, CONTACTS_TABLE_NAME);
+        return numRows;
+    }
+
+    public boolean updateContact (Integer id, String note)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("note", note);
+        db.update("notes", contentValues, "id = ? ", new String[] { Integer.toString(id) } );
+        return true;
+    }
+
+
+}
+
